7e8064fe16ebf942fea5dca20c5cdb6e0762eff2
Andrue Anderson <andrue.anderson.cs@gmail.com>
2026-08-18 08:10:05 -0400

    share program matcher index adapter

--- packages/matchers/src/builtins/functionalCoreEffect/functionalCoreEffectIndexBuild.ts
+++ packages/matchers/src/builtins/functionalCoreEffect/functionalCoreEffectIndexBuild.ts
@@ -1,7 +1,6 @@
-import { flow } from "effect"
-import { makeMatcherFromSubscriptions } from "../../matcher/makeMatcherFromSubscriptions.js"
 import type { Subscription } from "../../matcher/subscription.js"
 import type { ProgramContext } from "@better-typescript/matchers/sources/data"
+import { withProgramMatcherIndex } from "../../matcher/withProgramMatcherIndex.js"
 import { roleMapFromProgram } from "../../support/roleMapFromProgram.js"
 import type { FunctionalCoreEffectPolicy } from "./functionalCoreEffectPolicyClass.js"
 import { FunctionalCoreEffectIndex } from "./functionalCoreEffectIndexClass.js"
@@ -20,4 +19,4 @@ export const buildFunctionalCoreEffectIndex =
 export const withFunctionalCoreEffectIndex =
   <Fact>(subscriptions: (index: FunctionalCoreEffectIndex) => ReadonlyArray<Subscription<Fact>>) =>
   (policy: FunctionalCoreEffectPolicy) =>
-    makeMatcherFromSubscriptions(flow(buildFunctionalCoreEffectIndex(policy), subscriptions))
+    withProgramMatcherIndex(buildFunctionalCoreEffectIndex(policy))(subscriptions)
